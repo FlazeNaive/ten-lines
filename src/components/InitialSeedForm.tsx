fbd089b55e9a2a894cfa31d7da39f39febeb196a
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2026-05-28 16:23:12 -0600

    [fix] disable TTV entry when switch game is selected

--- src/components/InitialSeedForm.tsx
+++ src/components/InitialSeedForm.tsx
@@ -228,7 +228,7 @@ export default function TenLinesForm({
                     ]
                 }
             </TextField>
-            {isFRLG && (
+            {isFRLG && !isSwitch && (
                 <TeachyTVEntry
                     isTeachyTVMode={isTeachyTVMode}
                     teachyTVRegularOut={teachyTVRegularOut}
